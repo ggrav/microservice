4174bd6b5a730893dee5ce03cefcfd3dfd66efd2
Gaurav <gaurav2cool@gmail.com>
2014-10-27 15:26:50 +0530

    dummy changes

--- TestMicroservice/TestMicroservice/Controllers/ValuesController.cs
+++ TestMicroservice/TestMicroservice/Controllers/ValuesController.cs
@@ -12,13 +12,13 @@ namespace TestMicroservice.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2","testValue" };
         }
 
         // GET api/values/5
-        public string Get(int id)
+        public string Get(int id) 
         {
-            return "value";
+            return "newValue";
         }
 
         // POST api/values
